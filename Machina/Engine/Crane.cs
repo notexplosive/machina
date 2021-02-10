480e886c1e6c4db8cb28d612db9762728c4bdcd2
notexplosive <thenotexplosive@gmail.com>
2021-02-10 06:59:06 -0800

    Added docstrings for Crane.cs functions

--- Machina/Engine/Crane.cs
+++ Machina/Engine/Crane.cs
@@ -9,14 +9,54 @@ namespace Machina.Engine
 {
     public interface ICrane
     {
+        /// <summary>
+        /// Called every frame with the delta time since last frame.
+        /// </summary>
+        /// <param name="dt">Delta time since last frame</param>
         public void Update(float dt);
+        /// <summary>
+        /// Called on every scene BEFORE any real drawing happens, does not imply spriteBatch.Start
+        /// </summary>
+        /// <param name="spriteBatch"></param>
         public void PreDraw(SpriteBatch spriteBatch);
+        /// <summary>
+        /// Called every visual frame, spriteBatch.Start has already been called.
+        /// </summary>
+        /// <param name="spriteBatch"></param>
         public void Draw(SpriteBatch spriteBatch);
+        /// <summary>
+        /// Happens after Draw if the DebugLevel is higher than Passive
+        /// </summary>
+        /// <param name="spriteBatch"></param>
         public void DebugDraw(SpriteBatch spriteBatch);
+        /// <summary>
+        /// Called every time the scroll wheel is incremented. Also subsequently calls MouseMove with zero delta.
+        /// </summary>
+        /// <param name="scrollDelta"></param>
         public void OnScroll(int scrollDelta);
+        /// <summary>
+        /// Called when the object is removed from its iterable set
+        /// </summary>
         public void OnRemove();
+        /// <summary>
+        /// Called when user presses or releases a key
+        /// </summary>
+        /// <param name="key">Key pressed or released</param>
+        /// <param name="buttonState">Enum for if the button was pressed or released</param>
+        /// <param name="modifiers">Modifiers that are currently pressed (ctrl, alt, shift)</param>
         public void OnKey(Keys key, ButtonState buttonState, ModifierKeys modifiers);
+        /// <summary>
+        /// Called every time mouse moves.
+        /// </summary>
+        /// <param name="currentPosition"></param>
+        /// <param name="positionDelta"></param>
         public void OnMouseMove(Point currentPosition, Vector2 positionDelta);
+        /// <summary>
+        /// Called when user presses or releases the mouse
+        /// </summary>
+        /// <param name="mouseButton">Mouse button pressed or released</param>
+        /// <param name="currentPosition"></param>
+        /// <param name="buttonState"></param>
         public void OnMouseButton(MouseButton mouseButton, Point currentPosition, ButtonState buttonState);
     }
 
@@ -64,7 +104,7 @@ namespace Machina.Engine
                 iterable.OnScroll(scrollDelta);
             }
 
-            this.OnMouseMove(Mouse.GetState().Position, new Vector2(0, 0));
+            this.OnMouseMove(Mouse.GetState().Position, Vector2.Zero);
         }
 
         public virtual void OnRemove()
