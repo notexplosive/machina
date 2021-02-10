88589164aaf04e7d59959adcf4f3c76e9aa890a2
notexplosive <thenotexplosive@gmail.com>
2021-02-09 18:43:02 -0800

    REFACTOR - Move struct to its own file

---
+++ Machina/Data/TweenAccessors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Machina.Data
+{
+    public struct TweenAccessors<T> where T : struct
+    {
+        public readonly Func<T> getter;
+        public readonly Action<T> setter;
+
+        public TweenAccessors(Func<T> getter, Action<T> setter)
+        {
+            this.getter = getter;
+            this.setter = setter;
+        }
+    }
+}
--- Machina/Data/TweenDataFunctions.cs
+++ Machina/Data/TweenDataFunctions.cs
@@ -26,16 +26,4 @@ namespace Machina.Data
             return new TweenAccessors<Vector2>(actor.GetPosition, actor.SetPosition);
         }
     }
-
-    public struct TweenAccessors<T> where T : struct
-    {
-        public readonly Func<T> getter;
-        public readonly Action<T> setter;
-
-        public TweenAccessors(Func<T> getter, Action<T> setter)
-        {
-            this.getter = getter;
-            this.setter = setter;
-        }
-    }
 }
