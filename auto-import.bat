22d0af749bf2cdc5a24a01455b4f7e8d37d6738f
notexplosive <thenotexplosive@gmail.com>
2021-05-24 17:27:57 -0700

    Update import scripts

---
+++ auto-import.bat
@@ -0,0 +1,21 @@
+@echo off
+if [%1]==[] goto usage
+
+git init
+git submodule add https://github.com/notexplosive/machina.git
+dotnet new mgdesktopgl -o %1
+dotnet new sln
+dotnet sln add %1
+dotnet sln add .\machina\Machina
+dotnet sln add .\machina\TestMachina
+dotnet add %1 reference .\machina\Machina\
+call .\machina\on-import.bat
+git add .
+git commit -m "(Machina:Automated) Initial Commit"
+pause
+goto :eof
+
+:usage
+@echo Usage: %0 ^<DesiredProjectName^>
+pause
+exit /B 1
--- on-import.bat
+++ on-import.bat
@@ -1,2 +1,5 @@
 @echo off
+REM This script gets run by the auto-import script from the outside
+
+REM Pull out the .gitignore, this will ignore a bunch of cruft as well as .demo files and the like
 xcopy .gitignore ..
