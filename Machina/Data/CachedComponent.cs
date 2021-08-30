3712573b0774953bbda4ba33be20416322120b8c
notexplosive <thenotexplosive@gmail.com>
2021-08-30 12:56:41 -0700

    CachedComponent can now ask if component exists

--- Machina/Data/CachedComponent.cs
+++ Machina/Data/CachedComponent.cs
@@ -20,14 +20,23 @@ namespace Machina.Data
 
         public T GetComponent()
         {
+            AttemptCacheComponent();
+            Debug.Assert(HasComponent());
+            return this.component;
+        }
+
+        public bool HasComponent()
+        {
+            AttemptCacheComponent();
+            return this.component != null;
+        }
+
+        private void AttemptCacheComponent()
+        {
             if (this.component == null)
             {
                 this.component = this.actor.GetComponent<T>();
             }
-
-            Debug.Assert(this.component != null);
-
-            return this.component;
         }
     }
 }
