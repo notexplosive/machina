d1836b9f125149fa0892a09e77506ec5dc6806c0
notexplosive <thenotexplosive@gmail.com>
2021-11-17 09:35:07 -0800

    Extract file

---
+++ Machina/Engine/MachinaClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Machina.Data;
+using Machina.Engine.Debugging.Data;
+
+namespace Machina.Engine
+{
+    using Assets;
+
+    public static class MachinaClient
+    {
+        public static MachinaRuntime Runtime { get; private set; }
+        public static SoundEffectPlayer SoundEffectPlayer { get; private set; }
+        public static NoiseBasedRNG RandomDirty { get; } = new NoiseBasedRNG((uint) DateTime.Now.Ticks & 0x0000FFFF);
+        public static FrameStep GlobalFrameStep { get; } = new FrameStep();
+        public static IAssetLibrary Assets { get; private set; } = new EmptyAssetLibrary();
+
+        public static void Setup(MachinaRuntime runtime, IAssetLibrary assetLibrary)
+        {
+            Runtime = runtime;
+            SoundEffectPlayer = new SoundEffectPlayer(runtime.Settings);
+            Assets = assetLibrary;
+        }
+
+        /// <summary>
+        ///     Print to the in-game debug console
+        /// </summary>
+        /// <param name="objects">Arbitrary list of any objects, converted with .ToString and delimits with spaces.</param>
+        public static void Print(params object[] objects)
+        {
+            Runtime?.CurrentCartridge?.Logger.Log(objects);
+            new StdOutConsoleLogger().Log(objects);
+        }
+    }
+}
--- Machina/Engine/MachinaGame.cs
+++ Machina/Engine/MachinaGame.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using Machina.Components;
 using Machina.Data;
-using Machina.Engine.Debugging.Data;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -17,32 +16,6 @@ namespace Machina.Engine
         Off, // Completely disabled, can be enabled with hotkey
         Passive, // Show Console Output
         Active // Render DebugDraws
-    }
-
-    public static class MachinaClient
-    {
-        public static MachinaRuntime Runtime { get; private set; }
-        public static SoundEffectPlayer SoundEffectPlayer { get; private set; }
-        public static NoiseBasedRNG RandomDirty { get; } = new NoiseBasedRNG((uint) DateTime.Now.Ticks & 0x0000FFFF);
-        public static FrameStep GlobalFrameStep { get; } = new FrameStep();
-        public static IAssetLibrary Assets { get; private set; } = new EmptyAssetLibrary();
-
-        public static void Setup(MachinaRuntime runtime, IAssetLibrary assetLibrary)
-        {
-            Runtime = runtime;
-            SoundEffectPlayer = new SoundEffectPlayer(runtime.Settings);
-            Assets = assetLibrary;
-        }
-
-        /// <summary>
-        ///     Print to the in-game debug console
-        /// </summary>
-        /// <param name="objects">Arbitrary list of any objects, converted with .ToString and delimits with spaces.</param>
-        public static void Print(params object[] objects)
-        {
-            Runtime?.CurrentCartridge?.Logger.Log(objects);
-            new StdOutConsoleLogger().Log(objects);
-        }
     }
 
     public class MachinaGame : Game
