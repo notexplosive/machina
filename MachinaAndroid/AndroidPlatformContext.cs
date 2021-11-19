89cba2043a0b9ce44553db465d2d9c3df2a14acb
notexplosive <thenotexplosive@gmail.com>
2021-11-19 08:43:25 -0800

    Create MachinaAndroid.csproj

---
+++ MachinaAndroid/AndroidPlatformContext.cs
@@ -0,0 +1,14 @@
+﻿using Machina.Engine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MachinaAndroid
+{
+    public class AndroidPlatformContext : IPlatformContext
+    {
+        public void OnCartridgeSetup(Cartridge cartridge, MachinaWindow window)
+        {
+        }
+    }
+}
