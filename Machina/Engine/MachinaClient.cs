644cc7a33d2c00020ad4125cedd4d93941cc2453
notexplosive <thenotexplosive@gmail.com>
2021-11-26 11:02:08 -0800

    Comments

--- Machina/Engine/MachinaClient.cs
+++ Machina/Engine/MachinaClient.cs
@@ -6,10 +6,13 @@ namespace Machina.Engine
 {
     using Assets;
 
+    /// <summary>
+    /// Static global accessor for all of the static global components in the game.
+    /// </summary>
     public static class MachinaClient
     {
-        public static MachinaRuntime Runtime { get; private set; }
-        public static SoundEffectPlayer SoundEffectPlayer { get; private set; }
+        public static MachinaRuntime Runtime { get; private set; } // Runtime should not be here!
+        public static SoundEffectPlayer SoundEffectPlayer { get; private set; } // Needs to be initialzed, should not be here!
         public static NoiseBasedRNG RandomDirty { get; } = new NoiseBasedRNG((uint) DateTime.Now.Ticks & 0x0000FFFF);
         public static FrameStep GlobalFrameStep { get; } = new FrameStep();
         public static IAssetLibrary Assets { get; private set; } = new EmptyAssetLibrary();
