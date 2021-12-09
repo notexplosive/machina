4e9d899effefda80be9c5aa6321a421d6831bb1d
notexplosive <thenotexplosive@gmail.com>
2021-12-08 21:13:24 -0800

    Extract file

---
+++ Machina/Data/Layout/LayoutIntermediate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace Machina.Data.Layout
+{
+    public class LayoutIntermediate
+    {
+        public readonly Dictionary<ILayoutEdge, int> sizeLookupTable = new Dictionary<ILayoutEdge, int>();
+        public LayoutResult LayoutResult { get; }
+
+        public LayoutIntermediate(LayoutNode rootNode)
+        {
+            this.LayoutResult = new LayoutResult(new LayoutResultNode(Point.Zero, GetMeasuredSize(rootNode.Size), 0));
+        }
+
+        public int MeasureEdge(ILayoutEdge edge)
+        {
+            if (!edge.IsStretched)
+            {
+                return edge.ActualSize;
+            }
+
+            return sizeLookupTable[edge];
+        }
+
+        public Point GetMeasuredSize(LayoutSize size)
+        {
+            return new Point(MeasureEdge(size.X), MeasureEdge(size.Y));
+        }
+
+        public void AddLayoutNode(Point position, LayoutNode node, int nestingLevel)
+        {
+            if (node.Name.Exists)
+            {
+                this.LayoutResult.Add(node.Name.Text, new LayoutResultNode(position, GetMeasuredSize(node.Size), nestingLevel));
+            }
+        }
+    }
+}
--- Machina/Data/Layout/LayoutResult.cs
+++ Machina/Data/Layout/LayoutResult.cs
@@ -1,42 +1,8 @@
-﻿using Microsoft.Xna.Framework;
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace Machina.Data.Layout
 {
-    public class LayoutIntermediate
-    {
-        public readonly Dictionary<ILayoutEdge, int> sizeLookupTable = new Dictionary<ILayoutEdge, int>();
-        public LayoutResult LayoutResult { get; }
-
-        public LayoutIntermediate(LayoutNode rootNode)
-        {
-            this.LayoutResult = new LayoutResult(new LayoutResultNode(Point.Zero, GetMeasuredSize(rootNode.Size), 0));
-        }
-
-        public int MeasureEdge(ILayoutEdge edge)
-        {
-            if (!edge.IsStretched)
-            {
-                return edge.ActualSize;
-            }
-
-            return sizeLookupTable[edge];
-        }
-
-        public Point GetMeasuredSize(LayoutSize size)
-        {
-            return new Point(MeasureEdge(size.X), MeasureEdge(size.Y));
-        }
-
-        public void AddLayoutNode(Point position, LayoutNode node, int nestingLevel)
-        {
-            if (node.Name.Exists)
-            {
-                this.LayoutResult.Add(node.Name.Text, new LayoutResultNode(position, GetMeasuredSize(node.Size), nestingLevel));
-            }
-        }
-    }
 
     public class LayoutResult
     {
