a2351d517ed37143a980871f4fcfa48b6fcb7ef0
notexplosive <thenotexplosive@gmail.com>
2021-12-19 22:51:33 -0800

    Ascii Draw Panel supports drawing lines

--- TestMachina/Utility/AsciiDrawPanel.cs
+++ TestMachina/Utility/AsciiDrawPanel.cs
@@ -8,22 +8,22 @@ namespace TestMachina.Utility
 {
     public class AsciiDrawPanel
     {
-        private readonly Point size;
+        public Point Size { get; }
         private readonly char[,] content;
         private List<CharAndPostion> offscreenPixels = new List<CharAndPostion>();
 
         public AsciiDrawPanel(Point size)
         {
-            this.size = size;
-            this.content = new char[this.size.X, this.size.Y];
+            this.Size = size;
+            this.content = new char[this.Size.X, this.Size.Y];
             Clear(' ');
         }
 
         public void Clear(char pixel)
         {
-            for (int y = 0; y < this.size.Y; y++)
+            for (int y = 0; y < this.Size.Y; y++)
             {
-                for (int x = 0; x < this.size.X; x++)
+                for (int x = 0; x < this.Size.X; x++)
                 {
                     this.content[x, y] = pixel;
                 }
@@ -32,7 +32,7 @@ namespace TestMachina.Utility
 
         public void DrawPixelAt(Point position, char pixel)
         {
-            if (position.X < 0 || position.Y < 0 || position.X >= size.X || position.Y >= size.Y)
+            if (position.X < 0 || position.Y < 0 || position.X >= Size.X || position.Y >= Size.Y)
             {
                 offscreenPixels.Add(new CharAndPostion(position, pixel));
                 return;
@@ -69,9 +69,9 @@ namespace TestMachina.Utility
         public string GetImage()
         {
             StringBuilder result = new StringBuilder();
-            for (int y = 0; y < this.size.Y; y++)
+            for (int y = 0; y < this.Size.Y; y++)
             {
-                for (int x = 0; x < this.size.X; x++)
+                for (int x = 0; x < this.Size.X; x++)
                 {
                     result.Append(this.content[x, y]);
                 }
@@ -101,6 +101,38 @@ namespace TestMachina.Utility
 
             public char Char { get; }
             public Point Position { get; }
+        }
+
+        public void DrawLine(Point start, Point end, char text)
+        {
+            var startAsVector = start.ToVector2();
+            var relativeEndAsVector = end.ToVector2() - startAsVector;
+
+            var distance = relativeEndAsVector.Length();
+
+            for (int i = 0; i < distance; i++)
+            {
+                var lerp = Vector2.Lerp(Vector2.Zero, relativeEndAsVector, (float)i / distance);
+                var location = (startAsVector + lerp).ToPoint();
+                DrawPixelAt(location, text);
+            }
+        }
+
+        public void DrawCanvasAt(Point pos, AsciiDrawPanel innerPanel)
+        {
+            for (int x = 0; x < innerPanel.Size.X; x++)
+            {
+                for (int y = 0; y < innerPanel.Size.Y; y++)
+                {
+                    var innerPanelPixel = innerPanel.content[x, y];
+                    DrawPixelAt(new Point(pos.X + x, pos.Y + y), innerPanelPixel);
+                }
+            }
+
+            foreach (var offscreenPixel in innerPanel.offscreenPixels)
+            {
+                this.offscreenPixels.Add(offscreenPixel);
+            }
         }
     }
 }
