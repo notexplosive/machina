3d8fc845fcc7d0506fdb05ed579ee3adab3f3106
notexplosive <thenotexplosive@gmail.com>
2022-02-02 08:11:59 -0800

    t   Very basic pinning test for BoundedText

---
+++ TestMachina/Tests/BoundedTextTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Machina.Components;
+using Machina.Data;
+using Machina.Data.TextRendering;
+using Machina.Engine;
+using Microsoft.Xna.Framework;
+using Xunit;
+
+namespace TestMachina.Tests
+{
+    public class BoundedTextTests
+    {
+        [Fact]
+        public void bounded_text_renderer_generates_accurate_output()
+        {
+            var textMeasurer = new TextMeasurer(
+                "Hello world",
+                new MonospacedFontMetrics(new Point(2, 3)),
+                new Rectangle(Point.Zero, new Point(200, 200)),
+                HorizontalAlignment.Center,
+                VerticalAlignment.Center,
+                Overflow.Ignore);
+
+            textMeasurer.Lines.Should().HaveCount(1);
+
+            textMeasurer.Lines[0].textPosition.Should().Be(new Point(89, 0));
+        }
+    }
+}
