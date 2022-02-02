4a117d6e08f72067f2ffc53de895ef108f8a2340
notexplosive <thenotexplosive@gmail.com>
2022-02-02 08:01:13 -0800

    F   Add API for MonospacedFontMetrics

---
+++ Machina/Data/TextRendering/MonospacedFontMetrics.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Machina.Data.TextRendering
+{
+    public class MonospacedFontMetrics : IFontMetrics
+    {
+        public Point Size { get; }
+
+        public MonospacedFontMetrics(Point size)
+        {
+            this.Size = size;
+        }
+
+        public int LineSpacing => Size.Y;
+
+        public Vector2 MeasureString(string text)
+        {
+            return new Vector2(Size.X * text.Length, LineSpacing);
+        }
+    }
+}
