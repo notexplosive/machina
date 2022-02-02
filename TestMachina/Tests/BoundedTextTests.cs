8fa610486e1dfe94e3ba41829fc5f60ad15867e4
notexplosive <thenotexplosive@gmail.com>
2022-02-02 09:02:02 -0800

    t   pinning tests for TextMeasurer

--- TestMachina/Tests/BoundedTextTests.cs
+++ TestMachina/Tests/BoundedTextTests.cs
@@ -29,14 +29,28 @@ namespace TestMachina.Tests
             localTextPos.Should().Be(new Point(expectedX, 99));
         }
 
-        /*
         [Fact]
         public void pinning_tests_for_text_measurer()
         {
             var fontMetrics = new MonospacedFontMetrics(new Point(2, 3));
 
-            new TextMeasurer("This is a very long string. I thought about referencing some meme here in this string.\nBut then I changed my mind.", fontMetrics, );
+            var textMeasurer = new TextMeasurer(
+                "This is a very long string. I thought about referencing some meme here in this string.\nBut then I changed my mind.",
+                fontMetrics,
+                new Rectangle(Point.Zero, new Point(100, 200)),
+                HorizontalAlignment.Center,
+                VerticalAlignment.Center,
+                Overflow.Elide);
+
+            textMeasurer.Lines.Should().HaveCount(3);
+            textMeasurer.Lines[0].textContent.Should().Be("This is a very long string. I thought about ");
+            textMeasurer.Lines[0].textPosition.Y.Should().Be(0);
+
+            textMeasurer.Lines[1].textContent.Should().Be("referencing some meme here in this string. ");
+            textMeasurer.Lines[1].textPosition.Y.Should().Be(3);
+
+            textMeasurer.Lines[2].textContent.Should().Be("But then I changed my mind. ");
+            textMeasurer.Lines[2].textPosition.Y.Should().Be(6);
         }
-        */
     }
 }
