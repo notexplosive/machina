c99516d5857ef416dd6be12f40c29fc41573eadc
notexplosive <thenotexplosive@gmail.com>
2022-02-02 07:56:49 -0800

    r   Move class to own file

---
+++ Machina/Data/TextRendering/TextLine.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Machina.Data.TextRendering
+{
+    public readonly struct TextLine
+    {
+        public readonly string textContent;
+        public readonly Point textPosition;
+
+        public TextLine(string content, SpriteFontMetrics fontMetrics, Rectangle bounds, int positionY,
+            HorizontalAlignment horizontalAlignment)
+        {
+            this.textContent = content;
+            this.textPosition = new Point(0, positionY);
+
+            if (horizontalAlignment == HorizontalAlignment.Left)
+            {
+                this.textPosition.X = bounds.Location.X;
+            }
+            else if (horizontalAlignment == HorizontalAlignment.Right)
+            {
+                var widthOffset = bounds.Width - (int)fontMetrics.MeasureString(content).X + 1;
+                this.textPosition.X = bounds.Location.X + widthOffset;
+            }
+            else
+            {
+                var widthOffset = bounds.Width - (int)fontMetrics.MeasureString(content).X / 2 + 1 - bounds.Width / 2;
+                this.textPosition.X = bounds.Location.X + widthOffset;
+            }
+        }
+    }
+}
