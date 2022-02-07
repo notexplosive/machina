2d1a58d834ec17d252a543eef40353b20b774013
notexplosive <thenotexplosive@gmail.com>
2022-02-06 16:19:46 -0800

    t   Pinning test for BakedLayout

---
+++ TestMachina/Tests/BakedLayoutTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Machina.Data;
+using Machina.Data.Layout;
+using Microsoft.Xna.Framework;
+using Xunit;
+
+namespace TestMachina.Tests
+{
+    public class BakedLayoutTests
+    {
+        [Fact]
+        public void can_get_baked_layout_node_by_name()
+        {
+            var layout = LayoutNode.HorizontalParent("root", LayoutSize.Pixels(20, 5), new LayoutStyle(margin: new Point(1, 1), padding: 1, Alignment.TopLeft),
+                LayoutNode.Leaf("a", LayoutSize.StretchedBoth()),
+                LayoutNode.Leaf("b", LayoutSize.StretchedBoth()),
+                LayoutNode.Leaf("c", LayoutSize.StretchedBoth())
+            );
+
+            var subject = layout.Bake();
+
+            var acquiredNode = subject.GetNode("b");
+            acquiredNode.Size.Should().Be(new Point(5, 3));
+        }
+    }
+}
