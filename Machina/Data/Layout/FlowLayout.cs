9c2dad036165eb1600799ea484dfaeb7206fc232
notexplosive <thenotexplosive@gmail.com>
2022-02-06 17:55:45 -0800

    F!! FlowLayout Exists

---
+++ Machina/Data/Layout/FlowLayout.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Machina.Data.Layout
+{
+    public static class FlowLayout
+    {
+        private class Rows
+        {
+            public Rows(int defaultWidth)
+            {
+                DefaultWidth = defaultWidth;
+
+                CurrentRow = new Row(DefaultWidth);
+                Content.Add(CurrentRow);
+            }
+
+            public Row CurrentRow { get; private set; }
+            public int DefaultWidth { get; }
+            public List<Row> Content { get; } = new List<Row>();
+
+            public void CreateNextRowAndAdd(LayoutNode child)
+            {
+                CurrentRow = new Row(DefaultWidth);
+                CurrentRow.AddItem(child);
+                Content.Add(CurrentRow);
+            }
+
+            public LayoutNode[] GetLayoutNodesOfEachRow()
+            {
+                var nodes = new LayoutNode[Content.Count];
+
+                for (int i = 0; i < Content.Count; i++)
+                {
+                    nodes[i] = Content[i].GetLayoutNode($"row {i}");
+                }
+
+                return nodes;
+            }
+        }
+
+        private class Row
+        {
+            public Row(int width)
+            {
+                TotalWidth = width;
+                RemainingWidth = width;
+            }
+
+            public List<LayoutNode> Content { get; } = new List<LayoutNode>();
+            public int Height { get; private set; }
+            public int TotalWidth { get; }
+            public int RemainingWidth { get; private set; }
+
+            public void AddItem(LayoutNode child)
+            {
+                RemainingWidth -= child.Size.Width.ActualSize;
+                Height = Math.Max(Height, child.Size.Height.ActualSize);
+                Content.Add(child);
+            }
+
+            public LayoutNode GetLayoutNode(string rowNodeName)
+            {
+                return LayoutNode.HorizontalParent(rowNodeName, LayoutSize.Pixels(TotalWidth - RemainingWidth, Height), LayoutStyle.Empty, Content.ToArray());
+            }
+        }
+
+        // Should eventually be called "HorizontalLeftToRightFlowParent"
+        public static LayoutNode FlowParent(string name, LayoutSize size, LayoutStyle style, params LayoutNode[] children)
+        {
+            // "Horizontal"Parent here doesn't matter because we only have one child. It would be nice to have an API where we just say
+            // "parent of single thing" where we clarify orientation agnosticism when we guarantee only having one child
+            // While we're at it, it sucks that we have to give the node a name, then immediately ask for the node we just named
+            var workableArea = LayoutNode.HorizontalParent("throwAwayParent", size, style, LayoutNode.Leaf("workableArea", LayoutSize.StretchedBoth())).Bake().GetNode("workableArea");
+            var rows = new Rows(workableArea.Size.X);
+
+            foreach (var child in children)
+            {
+                if (rows.CurrentRow.RemainingWidth >= child.Size.Width.ActualSize)
+                {
+                    rows.CurrentRow.AddItem(child);
+                }
+                else
+                {
+                    rows.CreateNextRowAndAdd(child);
+                }
+            }
+
+            // again the root node being "Horizontal" doesn't matter
+            return LayoutNode.HorizontalParent(name, size, style,
+                LayoutNode.VerticalParent("rows", LayoutSize.StretchedBoth(), LayoutStyle.Empty,
+                    rows.GetLayoutNodesOfEachRow()
+                )
+            );
+        }
+    }
+}
---
+++ TestMachina/Tests/FlowLayoutTests.cs
@@ -0,0 +1,28 @@
+﻿using ApprovalTests;
+using ApprovalTests.Reporters;
+using Machina.Data.Layout;
+using TestMachina.Utility;
+using Xunit;
+
+namespace TestMachina.Tests
+{
+    [UseReporter(typeof(DiffReporter))]
+    public class FlowLayoutTests
+    {
+        [Fact]
+        public void can_create_flow_layout()
+        {
+            var layout = FlowLayout.FlowParent("root", LayoutSize.Pixels(30, 30), LayoutStyle.Empty,
+                LayoutNode.Leaf("itemA", LayoutSize.Pixels(10, 10)),
+                LayoutNode.Leaf("itemB", LayoutSize.Pixels(10, 10)),
+                LayoutNode.Leaf("itemC", LayoutSize.Pixels(10, 10)),
+                LayoutNode.Leaf("itemD", LayoutSize.Pixels(10, 10)),
+                LayoutNode.Leaf("itemE", LayoutSize.Pixels(10, 10)),
+                LayoutNode.Leaf("itemF", LayoutSize.Pixels(10, 10))
+            );
+
+            var result = layout.Bake();
+            Approvals.Verify(LayoutNodeUtils.DrawResult(result));
+        }
+    }
+}
