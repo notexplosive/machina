692f9098fce570112ce6a996f86742c78ee963ff
notexplosive <thenotexplosive@gmail.com>
2022-02-08 09:56:49 -0800

    r   Move class to own file

--- Machina/Data/Layout/RawLayout.cs
+++ Machina/Data/Layout/RawLayout.cs
@@ -3,30 +3,6 @@ using System;
 
 namespace Machina.Data.Layout
 {
-    public abstract class AbstractRawLayout<BakedType> where BakedType : IBakedLayout
-    {
-        public LayoutNode RootNode { get; }
-        private LayoutBaker Baker { get; }
-
-        public AbstractRawLayout(LayoutNode rootNode)
-        {
-            RootNode = rootNode;
-            Baker = new LayoutBaker(RootNode);
-        }
-
-        public BakedLayout DefaultBake()
-        {
-            return Baker.Bake();
-        }
-
-        public static implicit operator LayoutNode(AbstractRawLayout<BakedType> self)
-        {
-            return self.RootNode;
-        }
-
-        public abstract BakedType Bake();
-    }
-
     public class RawLayout : AbstractRawLayout<BakedLayout>
     {
         public RawLayout(LayoutNode rootNode) : base(rootNode)
