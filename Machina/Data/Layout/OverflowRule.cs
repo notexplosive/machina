8920470a407b8e2be384f0240d36c127d83af3ea
notexplosive <thenotexplosive@gmail.com>
2022-02-07 20:33:19 -0800

    r   Move class to own file

---
+++ Machina/Data/Layout/OverflowRule.cs
@@ -0,0 +1,21 @@
+﻿namespace Machina.Data.Layout
+{
+    public class OverflowRule
+    {
+        private OverflowRule(bool haltOnFailure, bool deletesWholeRow, bool doNotAddMoreRowsAfterFailure)
+        {
+            HaltImmediatelyUponFailure = haltOnFailure;
+            DeletesWholeRowUponFailure = deletesWholeRow;
+            DoNotAddMoreRowsAfterFailure = doNotAddMoreRowsAfterFailure;
+        }
+
+        public static OverflowRule PermitExtraRows = new OverflowRule(false, false, false);
+        public static OverflowRule HaltOnIllegal = new OverflowRule(true, false, true);
+        public static OverflowRule CancelRowOnIllegal = new OverflowRule(true, true, true);
+        public static OverflowRule FinishRowOnIllegal = new OverflowRule(false, false, true);
+
+        public bool HaltImmediatelyUponFailure { get; }
+        public bool DeletesWholeRowUponFailure { get; }
+        public bool DoNotAddMoreRowsAfterFailure { get; }
+    }
+}
