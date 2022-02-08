09588ae7aebb380a4c43948dbe569f1816ce0046
notexplosive <thenotexplosive@gmail.com>
2022-02-08 08:00:31 -0800

    r   Move class to own file

--- Machina/Data/Layout/LayoutNode.cs
+++ Machina/Data/Layout/LayoutNode.cs
@@ -5,28 +5,6 @@ using System.Text;
 
 namespace Machina.Data.Layout
 {
-    public class RawLayout
-    {
-        public RawLayout(LayoutNode rootNode)
-        {
-            RootNode = rootNode;
-            Baker = new LayoutBaker(RootNode);
-        }
-
-        public LayoutNode RootNode { get; }
-        private LayoutBaker Baker { get; }
-
-        public BakedLayout Bake()
-        {
-            return Baker.Bake();
-        }
-
-        public static implicit operator LayoutNode(RawLayout self)
-        {
-            return self.RootNode;
-        }
-    }
-
     public class LayoutNode
     {
         /// <summary>
