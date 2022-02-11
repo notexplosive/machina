544a5cb18aef619dda5ebf5c9589abd5da363128
notexplosive <thenotexplosive@gmail.com>
2022-02-10 18:00:04 -0800

    t   Add test for FormattedText

---
+++ TestMachina/Tests/FormattedTextTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Machina.Data.TextRendering;
+using Microsoft.Xna.Framework;
+using Xunit;
+
+namespace TestMachina.Tests
+{
+    public class FormattedTextTests
+    {
+        [Fact]
+        public void formatted_text_pinning()
+        {
+            var font1 = new MonospacedFontMetrics(new Point(4, 4));
+            var font2 = new MonospacedFontMetrics(new Point(4, 5));
+
+            var subject = new FormattedText(new FormattedTextFragment("Hello in blue. ", font1, Color.Blue), new FormattedTextFragment("Hello in red!", font2, Color.Red));
+
+            subject.GetAllTokens().Should().HaveCount(11);
+        }
+    }
+}
