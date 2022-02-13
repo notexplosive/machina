810eae3465edd3e9f6da1064087970ab8d96363d
notexplosive <thenotexplosive@gmail.com>
2022-02-12 22:04:55 -0800

    r   move class to own file

--- Machina/Data/TextRendering/IDrawableTextElement.cs
+++ Machina/Data/TextRendering/IDrawableTextElement.cs
@@ -4,25 +4,6 @@ using System;
 
 namespace Machina.Data.TextRendering
 {
-    public struct TextDrawingArgs
-    {
-        public Point Origin { get; set; }
-        public Point Position { get; set; }
-        public float Angle { get; set; }
-        public Point AdditionalOffset { get; set; }
-        public Depth Depth { get; set; }
-
-        public Vector2 ResultOrigin()
-        {
-            return Origin.ToVector2();
-        }
-
-        public Vector2 ResultOffset()
-        {
-            return AdditionalOffset.ToVector2() - Position.ToVector2();
-        }
-    }
-
     public interface IDrawableTextElement
     {
         public string TokenText { get; }
---
+++ Machina/Data/TextRendering/TextDrawingArgs.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Machina.Data.TextRendering
+{
+    public struct TextDrawingArgs
+    {
+        public Point Origin { get; set; }
+        public Point Position { get; set; }
+        public float Angle { get; set; }
+        public Point AdditionalOffset { get; set; }
+        public Depth Depth { get; set; }
+
+        public Vector2 ResultOrigin()
+        {
+            return Origin.ToVector2();
+        }
+
+        public Vector2 ResultOffset()
+        {
+            return AdditionalOffset.ToVector2() - Position.ToVector2();
+        }
+    }
+}
