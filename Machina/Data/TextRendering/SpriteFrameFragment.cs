9dd5af37a1686ed07c6ac99c59b5451fb427fdc9
notexplosive <thenotexplosive@gmail.com>
2022-02-12 23:00:25 -0800

    F** SpriteFrameFragment

---
+++ Machina/Data/TextRendering/SpriteFrameFragment.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Machina.Data.TextRendering
+{
+    public readonly struct SpriteFrameFragment : ITextInputFragment
+    {
+        private readonly SpriteFrame spriteFrame;
+
+        public SpriteFrameFragment(SpriteFrame spriteFrame)
+        {
+            this.spriteFrame = spriteFrame;
+        }
+
+        public FormattedTextToken[] Tokens()
+        {
+            return new FormattedTextToken[] { new FormattedTextToken(new ImageToken(this.spriteFrame.Size, DrawFunction)) };
+        }
+
+        private void DrawFunction(SpriteBatch spriteBatch, Rectangle bounds, Depth depth)
+        {
+            this.spriteFrame.Draw(spriteBatch, new Vector2(bounds.Left, bounds.Top), 1f, /*angle*/0, XYBool.False, depth, Color.White, false);
+        }
+    }
+}
