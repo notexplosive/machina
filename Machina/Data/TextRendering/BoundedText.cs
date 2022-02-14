18bdfdf1a97342002f2cadcf8f9507258b9ea627
notexplosive <thenotexplosive@gmail.com>
2022-02-14 09:00:33 -0800

    r   move struct to own file

--- Machina/Data/TextRendering/BoundedText.cs
+++ Machina/Data/TextRendering/BoundedText.cs
@@ -6,18 +6,6 @@ using System.Collections.Generic;
 
 namespace Machina.Data.TextRendering
 {
-    public readonly struct FragmentAndRow
-    {
-        public BakedFlowLayout.BakedRow BakedRow { get; }
-        public List<TextOutputFragment> Fragment { get; }
-
-        public FragmentAndRow(BakedFlowLayout.BakedRow bakedRow, List<TextOutputFragment> fragment)
-        {
-            BakedRow = bakedRow;
-            Fragment = fragment;
-        }
-    }
-
     public readonly struct BoundedText
     {
         private readonly Alignment alignment;
---
+++ Machina/Data/TextRendering/FragmentAndRow.cs
@@ -0,0 +1,17 @@
+﻿using Machina.Data.Layout;
+using System.Collections.Generic;
+
+namespace Machina.Data.TextRendering
+{
+    public readonly struct FragmentAndRow
+    {
+        public BakedFlowLayout.BakedRow BakedRow { get; }
+        public List<TextOutputFragment> Fragment { get; }
+
+        public FragmentAndRow(BakedFlowLayout.BakedRow bakedRow, List<TextOutputFragment> fragment)
+        {
+            BakedRow = bakedRow;
+            Fragment = fragment;
+        }
+    }
+}
