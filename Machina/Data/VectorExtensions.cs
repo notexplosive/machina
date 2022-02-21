c5d2853d82ca77b54911aa1602c4cc404c2a73b6
notexplosive <thenotexplosive@gmail.com>
2022-02-20 20:04:58 -0800

    F** Add VectorExtensions

---
+++ Machina/Data/VectorExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Machina.Data
+{
+    public static class VectorExtensions
+    {
+        public static Vector2 Polar(float length, float angle)
+        {
+            return new Vector2(length * MathF.Sin(angle), length * MathF.Cos(angle));
+        }
+
+        public static Vector2 Normalized(this Vector2 original)
+        {
+            var copy = original;
+            copy.Normalize();
+            return copy;
+        }
+    }
+}
